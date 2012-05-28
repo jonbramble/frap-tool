029a79a630e80ce98872d4fe92e025a0f07867e6
Jonathan Bramble <j.p.bramble@leeds.ac.uk>
2012-05-28 18:50:13 +0100

    added linear plot and script file

--- runfrap.sh
+++ runfrap.sh
@@ -1,2 +1,3 @@
 ./FRAP -p prima.tif -c closed.tif 10s.tif 20s.tif 30s.tif 40s.tif 50s.tif 1m.tif
 convert gaussian_file.ps -rotate 90 -size 1024x768 gaussian_file.jpg
+convert linear_file.ps -rotate 90 -size 1024x768 linear_file.jpg
