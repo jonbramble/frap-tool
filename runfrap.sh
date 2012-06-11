86ddfc3400b0e38e207c8e813d1af493e16a1380
Jonathan Bramble <j.p.bramble@leeds.ac.uk>
2012-06-11 10:19:47 +0100

    moved images to data directory

--- runfrap.sh
+++ runfrap.sh
@@ -1,3 +1,3 @@
-./FRAP -p prima.tif -c closed.tif 10s.tif 20s.tif 30s.tif 40s.tif 50s.tif 1m.tif 1m30s.tif 2m.tif 2m30s.tif
+./FRAP -p data/prima.tif -c data/closed.tif data/10s.tif data/20s.tif data/30s.tif data/40s.tif data/50s.tif data/1m.tif data/1m30s.tif data/2m.tif data/2m30s.tif
 convert gaussian_file.ps -rotate 90 -size 1024x768 gaussian_file.jpg
 convert linear_file.ps -rotate 90 -size 1024x768 linear_file.jpg
